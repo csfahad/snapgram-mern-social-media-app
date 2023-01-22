04e6367800bd3004c1a124593fb0796ff3360fb4
Cs Fahad <csfahadofficiall@gmail.com>
2023-01-22 16:43:01 +0530

    frontend_deploy✨

---
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,37 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+    const { palette } = useTheme();
+    const dark = palette.neutral.dark;
+    const main = palette.neutral.main;
+
+    return (
+        <WidgetWrapper>
+            <FlexBetween>
+                <Typography color={dark} variant="h5" fontWeight="500">
+                    Sponsored
+                </Typography>
+                <Typography color={main}>Create Ad</Typography>
+            </FlexBetween>
+            <img
+                width="100%"
+                height="auto"
+                alt="advert"
+                src={`${process.env.REACT_SERVER_URL}/assets/info4.jpeg`}
+                style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+            />
+            <FlexBetween>
+                <Typography color={dark}>MikaCosmetics</Typography>
+                <Typography color={main}>mikacosmetics.com</Typography>
+            </FlexBetween>
+            <Typography color={main} m="0.5rem 0">
+                Your pathway to stunning and immaculate beauty and made sure your skin
+                is exfoliating skin and shining like light.
+            </Typography>
+        </WidgetWrapper>
+    );
+};
+
+export default AdvertWidget;
